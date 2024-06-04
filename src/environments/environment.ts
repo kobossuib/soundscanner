d2838ead68c703ff8b5344bcfc5275ec7480433b
Toni Marin <tonimarinquesada@gmail.com>
2024-06-04 09:30:35 +0200

    manual html changes and environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
 
-    CALLBACK_URI: "https://soundscout-gh4uk8wnc-kobossuibs-projects.vercel.app/callback"
-
+    CALLBACK_URI: "http://localhost:4200/callback"
 };
